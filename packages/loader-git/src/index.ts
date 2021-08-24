0758ff3ddeb7a04e9dfab59f7cd4cad5d613d430
jjangga0214 <bnbcmindnpass@gmail.com>
2021-08-24 22:34:55 +0900

    feat(loader-git): impl recordGit

--- packages/loader-git/src/index.ts
+++ packages/loader-git/src/index.ts
@@ -1,9 +1,25 @@
 import childProcess from 'child_process'
 import path from 'path'
-import { getConfigDirnameFromEnvVar } from '@haetae/core'
+import memoizee from 'memoizee'
+import {
+  getConfigDirnameFromEnvVar,
+  getRecord,
+  HaetaeRecord,
+  HaetaePreRecord,
+} from '@haetae/core'
+import serialize from 'serialize-javascript'
+import fs from 'fs'
 
-function execAsync(command: string): Promise<string> {
-  return new Promise((resolve, reject) => {
+export const { name } = (() => {
+  const content = fs.readFileSync(
+    path.join(__dirname, '..', 'package.json'),
+    'utf8',
+  )
+  return JSON.parse(content)
+})()
+
+export const execAsync = (command: string): Promise<string> =>
+  new Promise((resolve, reject) => {
     childProcess.exec(command, {}, (err, stdout, stderr) => {
       if (stdout) {
         resolve(stdout)
@@ -11,11 +27,14 @@ function execAsync(command: string): Promise<string> {
       reject(err || stderr)
     })
   })
-}
 
 export interface LoadByGitChangedOptions {
-  gitSha?: string
+  gitSha?: string | Promise<string>
   rootDir?: string
+}
+
+type PatchedHaetaeRecord = HaetaeRecord & {
+  [name: string]: { gitSha: string }
 }
 
 /**
@@ -24,21 +43,55 @@ export interface LoadByGitChangedOptions {
  *   - an empth array if no change was made
  *   - null if gitSha is not given
  */
-export async function loadByGitChanged({
-  gitSha, // TODO: get default param from config, record
-  rootDir = getConfigDirnameFromEnvVar(),
-}: LoadByGitChangedOptions) {
-  if (!gitSha) {
-    return null
-  }
 
-  const command = `git diff --name-only ${gitSha}`
-  const res = await execAsync(command)
-  return res
-    .trim()
-    .split('\n')
-    .map((filename) => path.join(rootDir, filename))
+/**
+ * @memoized
+ */
+export const loadByGitChanged = memoizee(
+  async ({
+    gitSha = getRecord().then((r) => (r as PatchedHaetaeRecord)[name].gitSha),
+    rootDir = getConfigDirnameFromEnvVar(),
+  }: LoadByGitChangedOptions): Promise<string[]> => {
+    if (!gitSha) {
+      throw new Error('gitSha is invalid.')
+    }
+
+    const command = `git diff --name-only ${gitSha}`
+    const res = await execAsync(command)
+    return res
+      .trim()
+      .split('\n')
+      .map((filename) => path.join(rootDir, filename))
+  },
+  {
+    normalizer: serialize,
+  },
+)
+
+interface RecordGitOptions {
+  gitSha?: string | Promise<string>
 }
+
+/**
+ * @memoized
+ */
+export const recordGit = memoizee(
+  async ({
+    gitSha = execAsync('git rev-parse --verify HEAD').then((res) => res.trim()), // TODO: get default param from config, record
+  }: RecordGitOptions): Promise<HaetaePreRecord> => {
+    if (!gitSha) {
+      throw new Error('gitSha is invalid.')
+    }
+    return {
+      [name]: {
+        gitSha,
+      },
+    }
+  },
+  {
+    normalizer: serialize,
+  },
+)
 
 // ;(async (): Promise<void> => {
 // TODO: git submodule test
