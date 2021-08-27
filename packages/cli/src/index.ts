ce350308315f9c087a356c6482cf17a7368fab87
jjangga0214 <bnbcmindnpass@gmail.com>
2021-08-28 00:19:51 +0900

    feat(cli): add command `store` and subcommands `records`, `record`
    Plus, rename options case( -J to -j, -C to -c)

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,5 +1,6 @@
 import fs from 'fs'
 import path from 'path'
+import { program } from 'commander'
 import {
   setCurrentCommand,
   getConfig,
@@ -9,8 +10,8 @@ import {
   getStore,
   saveStore,
   getRecord,
+  getRecords,
 } from '@haetae/core'
-import { program } from 'commander'
 
 export const { version } = (() => {
   const content = fs.readFileSync(
@@ -25,14 +26,16 @@ async function main() {
 
   program
     .option(
-      '-C, --config [path]',
-      'config file directory or filename. It can be relative or absolute. (default: $HAETAE_CONFIG_FILE or ./haetae.config.js)',
+      '-c, --config [path]',
+      'config file directory or filename. (It can be relative or absolute.) (default: $HAETAE_CONFIG_FILE or ./haetae.config.js)',
     )
     .option(
-      '-J, --json',
+      '-j, --json',
       "print output to json if possible. (This doesn't affect to commands which has fixed format, or already json format)",
       false,
     )
+    .option('--no-color', 'disable colorized output', false)
+
   // .option(
   //   '-s, --store <path>',
   //   'Store file directory or filename. It can be relative or absolute.',
@@ -41,6 +44,10 @@ async function main() {
 
   program.parse(process.argv)
   const options = program.opts()
+  process.env.FORCE_COLOR = options.color ? '3' : '0'
+  // `chalk` (https://www.npmjs.com/package/chalk) use $FORCE_COLOR
+  // `chalk` should be dynamically imported after setting process.env.FORCE_COLOR
+  const chalk = (await import('chalk')).default
 
   const config = await (async () => {
     try {
@@ -54,19 +61,27 @@ async function main() {
   })()
 
   // todo: impl global error handler
-
+  const store = await getStore({ config })
+  program
+    .command('store')
+    .description('show content of store file')
+    .action(async () => {
+      console.log(JSON.stringify(store, null, 2))
+    })
   if (!config) {
     const message = 'Config file is not given.'
     if (options.json) {
       console.error(JSON.stringify({ result: 'error', message }, null, 2))
     } else {
-      console.error(`[ERROR] ${message}`)
+      console.error(`{red [ERROR]} ${message}`)
     }
   } else {
     // dynamically add commands and subcommands by reading config file.
     for (const command in config.commands) {
       if (Object.prototype.hasOwnProperty.call(config.commands, command)) {
-        const cmd = program.command(command)
+        const cmd = program
+          .command(command)
+          .description('user-defined command from config')
         for (const subCommand in config.commands[command]) {
           if (
             Object.prototype.hasOwnProperty.call(
@@ -76,15 +91,13 @@ async function main() {
           ) {
             const subCmd = cmd.command(subCommand)
             if (subCommand === 'env') {
-              subCmd
-                .description('print current env object')
-                .action(async () => {
-                  setCurrentCommand(command)
-                  const env = await invokeEnv({ config })
-                  console.log(JSON.stringify(env, null, 2))
-                })
+              subCmd.description('show current env object').action(async () => {
+                setCurrentCommand(command)
+                const env = await invokeEnv({ config })
+                console.log(JSON.stringify(env, null, 2))
+              })
             } else if (subCommand === 'target') {
-              subCmd.description('print target').action(async () => {
+              subCmd.description('show targets').action(async () => {
                 setCurrentCommand(command)
                 const targets = await invokeTarget({ config })
                 if (!options.json) {
@@ -95,11 +108,13 @@ async function main() {
               })
             } else if (subCommand === 'save') {
               subCmd
-                .description('save new record to store file')
+                .description(
+                  'save a new record to store file and show the new record',
+                )
                 .action(async () => {
                   setCurrentCommand(command)
                   const newStore = await mapStore({ config })
-                  saveStore({
+                  await saveStore({
                     store: newStore,
                     config,
                   })
@@ -115,6 +130,50 @@ async function main() {
             // }
           }
         }
+        cmd
+          .command('records')
+          .description('print every records of given command.')
+          .action(async () => {
+            setCurrentCommand(command)
+            const records = await getRecords({
+              store,
+            })
+            if (!records) {
+              const message = 'Not found from the store file.'
+              if (options.json) {
+                console.log(
+                  JSON.stringify({ result: 'error', message }, null, 2),
+                )
+              } else {
+                console.log(`{blue [INFO]} ${message}`)
+              }
+            } else {
+              console.log(JSON.stringify(records, null, 2))
+            }
+          })
+        cmd
+          .command('record')
+          .description('print record of given command and current env.')
+          .action(async () => {
+            setCurrentCommand(command)
+            const env = await invokeEnv({ config })
+            const record = await getRecord({
+              store,
+              env,
+            })
+            if (!record) {
+              const message = 'Not found from the store file.'
+              if (options.json) {
+                console.log(
+                  JSON.stringify({ result: 'error', message }, null, 2),
+                )
+              } else {
+                console.log(chalk`{blue [INFO]} ${message}`)
+              }
+            } else {
+              console.log(JSON.stringify(record, null, 2))
+            }
+          })
       }
     }
   }
