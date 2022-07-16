e547e18f5c43da3df059b4467010a831656a32a7
jjangga0214 <bnbcmindnpass@gmail.com>
2022-07-16 11:14:54 +0900

    feat(cli): add -d option

---
+++ .changeset/poor-bikes-drop.md
@@ -0,0 +1,5 @@
+---
+'@haetae/cli': patch
+---
+
+Added `-d` option for record data.
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -54,12 +54,18 @@ export async function run() {
         type: 'boolean',
         description: 'All/partial record(s)',
       },
+      d: {
+        alias: 'record-data',
+        type: 'boolean',
+        description: 'All/partial record(s) data',
+      },
       e: {
         alias: 'env',
         type: 'boolean',
         description: 'Current environment',
       },
     })
+    .conflicts('r', 'd')
     .example([
       [`$0 -c ./${defaultConfigFile} <...>`, 'Specify config file path.'],
       [
@@ -72,10 +78,15 @@ export async function run() {
       ],
       ['$0 -r', 'Show all content of the store file'],
       ['$0 -r <command>', 'Show all records of the given command'],
+      ['$0 -d <command>', 'Show all records data of the given command'],
       ['$0 -e <command>', 'Show current env of the given command'],
       [
         '$0 -r -e <command>',
         'Show a record of current env of the given command',
+      ],
+      [
+        '$0 -d -e <command>',
+        'Show a record data of current env of the given command',
       ],
     ])
 
@@ -109,10 +120,7 @@ export async function run() {
     // 3.1. When command is not given
 
     assert(!argv.e, 'Option "e" must be given with <command>')
-    assert(
-      argv.r === true,
-      'Option "r" must be given when <command> is not given',
-    )
+    assert(argv.r, 'Option "r" must be given when <command> is not given')
 
     console.log(JSON.stringify(await store(), undefined, 2))
   } else if (argv._.length === 1) {
@@ -125,13 +133,26 @@ export async function run() {
 
     if (argv.r && argv.e) {
       const record = await getRecord({ store: await store() })
+      // TODO: when undefined
       console.log(JSON.stringify(record, undefined, 2))
+    } else if (argv.d && argv.e) {
+      const record = await getRecord({ store: await store() })
+      // TODO: when undefined
+      console.log(JSON.stringify(record?.data, undefined, 2))
     } else if (argv.e) {
       const env = await invokeEnv()
       console.log(JSON.stringify(env, undefined, 2))
     } else if (argv.r) {
       const records = await getRecords({ store: await store() })
+      // TODO: when undefined or empty object
       console.log(JSON.stringify(records, undefined, 2))
+    } else if (argv.d) {
+      const records = await getRecords({ store: await store() })
+      // TODO: when undefined or empty object
+      if (records) {
+        const dataList = records.map((r) => r.data)
+        console.log(JSON.stringify(dataList, undefined, 2))
+      }
     } else {
       await saveStore({
         // if `argv.s` is undefined, `filename` will be resolved as default value
