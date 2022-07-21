5c9d20b1b7c282c4b1cbeabb936234233e0fc400
jjangga0214 <bnbcmindnpass@gmail.com>
2022-07-22 07:37:24 +0900

    feat(javascript): enable manual dependency graph
    Close #30

--- packages/javascript/src/index.ts
+++ packages/javascript/src/index.ts
@@ -7,16 +7,39 @@ export interface DependsOnOptions {
   tsConfig?: string
   rootDir?: string
   // TODO: more options
+  graph?: Record<string, string[]> // you can manually specify additional dependency graph
 }
 
+/**
+ *
+ * @param graph // You can specify any dependency graph regardless of extension
+ * { // When foo depends on bar and baz.
+ *   'path/to/foo.ts': ['path/to/bar.ts', 'path/to/baz.ts'],
+ * }
+ */
 export function dependsOn(
   filenames: readonly string[],
-  { tsConfig, rootDir = getConfigDirname() }: DependsOnOptions = {},
+  { tsConfig, rootDir = getConfigDirname(), graph = {} }: DependsOnOptions = {},
 ) {
   // default option.tsConfig if exists
   if (fs.existsSync(path.join(rootDir, 'tsconfig.json'))) {
     // eslint-disable-next-line no-param-reassign
     tsConfig = tsConfig || path.join(rootDir, 'tsconfig.json')
+  }
+  const absoluteGraph: Record<string, string[]> = {}
+  for (let file in graph) {
+    if (Object.prototype.hasOwnProperty.call(graph, file)) {
+      const deps = graph[file]
+      if (!path.isAbsolute(file)) {
+        file = path.join(rootDir, file)
+      }
+      absoluteGraph[file] = deps.map((dep) => {
+        if (!path.isAbsolute(dep)) {
+          return path.join(rootDir, dep)
+        }
+        return dep
+      })
+    }
   }
 
   return (target: string): boolean => {
@@ -28,6 +51,9 @@ export function dependsOn(
     })
     for (const filename of filenames) {
       if (deepDepsList.includes(filename)) {
+        return true
+      }
+      if (absoluteGraph[filename].includes(target)) {
         return true
       }
     }
