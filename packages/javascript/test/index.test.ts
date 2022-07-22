c83520149e45b90109cddc20863da30ecccbc4c1
jjangga0214 <bnbcmindnpass@gmail.com>
2022-07-22 12:47:38 +0900

    test(git): toIndexedDependencyRelationships

--- packages/javascript/test/index.test.ts
+++ packages/javascript/test/index.test.ts
@@ -1,26 +1,84 @@
 import path from 'path'
-import { dependsOn } from '@haetae/javascript'
+import { toIndexedDependencyRelationships, dependsOn } from '@haetae/javascript'
+
+function p(posixPath: string) {
+  return path.join(...posixPath.split('/'))
+}
 
 describe('index', () => {
-  it('dependsOn', async () => {
-    expect.hasAssertions()
-    const rootDir = path.join(__dirname, '..', '..', '..', 'test-project')
-    const predicate = await dependsOn(
-      [path.join(rootDir, 'packages', 'bar', 'src', 'index.ts')],
-      {
-        rootDir,
-      },
-    )
+  describe('dependsOn', () => {
+    test('basic usage', async () => {
+      expect.hasAssertions()
+      const rootDir = path.join(__dirname, '..', '..', '..', 'test-project')
+      const predicate = await dependsOn(
+        [path.join(rootDir, 'packages', 'bar', 'src', 'index.ts')],
+        {
+          rootDir,
+        },
+      )
 
-    const fooResult = predicate(
-      path.join(rootDir, 'packages', 'foo', 'test', 'unit', 'index.test.ts'),
-    )
+      const fooResult = predicate(
+        path.join(rootDir, 'packages', 'foo', 'test', 'unit', 'index.test.ts'),
+      )
 
-    expect(fooResult).toBe(false)
+      expect(fooResult).toBe(false)
 
-    const barResult = predicate(
-      path.join(rootDir, 'packages', 'bar', 'test', 'unit', 'index.test.ts'),
-    )
-    expect(barResult).toBe(true)
+      const barResult = predicate(
+        path.join(rootDir, 'packages', 'bar', 'test', 'unit', 'index.test.ts'),
+      )
+      expect(barResult).toBe(true)
+    })
+  })
+
+  describe('toIndexedDependencyRelationships', () => {
+    test('basic usage', () => {
+      const result = toIndexedDependencyRelationships({
+        rootDir: '<rootDir>',
+        relationships: [
+          {
+            dependents: [p('path/to/foo.ts')],
+            dependencies: [p('path/to/bar.ts'), p('path/to/baz.ts')],
+          },
+        ],
+      })
+      // TODO: path.join(rootDir, '')
+      expect(result).toStrictEqual({
+        [p('<rootDir>/path/to/foo.ts')]: new Set([
+          p('<rootDir>/path/to/bar.ts'),
+          p('<rootDir>/path/to/baz.ts'),
+        ]),
+      })
+    })
+    test('advanced usage', () => {
+      const result = toIndexedDependencyRelationships({
+        rootDir: '<rootDir>',
+        relationships: [
+          {
+            dependents: [p('path/to/foo.ts'), p('path/to/foo2.ts')],
+            dependencies: [p('path/to/bar.ts'), p('path/to/baz.ts')],
+          },
+          {
+            dependents: [p('path/to/foo2.ts'), p('path/to/foo3.ts')],
+            dependencies: [p('path/to/bar2.ts'), p('path/to/baz2.ts')],
+          },
+        ],
+      })
+      expect(result).toStrictEqual({
+        [p('<rootDir>/path/to/foo.ts')]: new Set([
+          p('<rootDir>/path/to/bar.ts'),
+          p('<rootDir>/path/to/baz.ts'),
+        ]),
+        [p('<rootDir>/path/to/foo2.ts')]: new Set([
+          p('<rootDir>/path/to/bar.ts'),
+          p('<rootDir>/path/to/baz.ts'),
+          p('<rootDir>/path/to/bar2.ts'),
+          p('<rootDir>/path/to/baz2.ts'),
+        ]),
+        [p('<rootDir>/path/to/foo3.ts')]: new Set([
+          p('<rootDir>/path/to/bar2.ts'),
+          p('<rootDir>/path/to/baz2.ts'),
+        ]),
+      })
+    })
   })
 })
