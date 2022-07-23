2057fc1d7ef13e00905327d0359c803334e4d04c
jjangga0214 <bnbcmindnpass@gmail.com>
2022-07-23 15:56:47 +0900

    fix(*): `pkg`
    Close #46

--- packages/utils/src/pkg.ts
+++ packages/utils/src/pkg.ts
@@ -1,18 +1,16 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-/* eslint-disable import/no-extraneous-dependencies */
-import { JSONSchemaForNPMPackageJsonFiles as PackageJson } from '@schemastore/package' // root devDep
+import path from 'path'
 import readPkg from 'read-pkg'
 import { major, minor, patch, prerelease } from 'semver'
 
-const pkg = readPkg.sync() as PackageJson
+const pkg = readPkg.sync({ cwd: path.join(__dirname, '..') })
 
 export default {
   name: '@haetae/utils',
   version: {
-    value: pkg.version!,
-    major: major(pkg.version!),
-    minor: minor(pkg.version!),
-    patch: patch(pkg.version!),
-    prerelease: prerelease(pkg.version!),
+    value: pkg.version,
+    major: major(pkg.version),
+    minor: minor(pkg.version),
+    patch: patch(pkg.version),
+    prerelease: prerelease(pkg.version),
   },
 }
