3a0f8323084e0f058b7deb2056ecf14ed15fc622
jjangga0214 <bnbcmindnpass@gmail.com>
2022-08-09 18:20:36 +0900

    docs: complete tabs for pkg managers

--- docs/components/PkgManagerTabs.tsx
+++ docs/components/PkgManagerTabs.tsx
@@ -1,5 +1,14 @@
-import { Tab, Box } from '@mui/material'
-import { TabPanel, TabList, TabContext } from '@mui/lab'
+import { Tab, Tabs, Box } from '@mui/material'
+import { TabPanel, TabContext } from '@mui/lab'
+import { useCallback } from 'react'
+import useSWR from 'swr'
+
+enum PkgManager {
+  NPM = 'npm',
+  YARN = 'yarn',
+  PNPM = 'pnpm',
+}
+const storageKey = 'PkgManager'
 
 // eslint-disable-next-line func-names
 export default function ({
@@ -7,19 +16,32 @@ export default function ({
 }: {
   children: JSX.Element[]
 }): JSX.Element {
-  const value = 'npm'
+  // Use SWR to sync every PkgManager choice.
+  const { data, mutate } = useSWR(storageKey, (key) =>
+    localStorage?.getItem(key),
+  )
+  const value = data || PkgManager.NPM
+
+  const onChange = useCallback(
+    (value: PkgManager) => {
+      localStorage.setItem(storageKey, value)
+      mutate(value, false)
+    },
+    [mutate],
+  )
+
   return (
     <TabContext value={value}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <TabList value="npm">
-          <Tab label="NPM" value="npm" />
-          <Tab label="YARN" value="yarn" />
-          <Tab label="PNPM" value="pnpm" />
-        </TabList>
+        <Tabs value={value} onChange={(_, _value) => onChange(_value)}>
+          <Tab label="NPM" value={PkgManager.NPM} />
+          <Tab label="YARN" value={PkgManager.YARN} />
+          <Tab label="PNPM" value={PkgManager.PNPM} />
+        </Tabs>
       </Box>
-      <TabPanel value="npm">{children[0]}</TabPanel>
-      <TabPanel value="yarn">{children[1]}</TabPanel>
-      <TabPanel value="pnpm">{children[2]}</TabPanel>
+      <TabPanel value={PkgManager.NPM}>{children[0]}</TabPanel>
+      <TabPanel value={PkgManager.YARN}>{children[1]}</TabPanel>
+      <TabPanel value={PkgManager.PNPM}>{children[2]}</TabPanel>
     </TabContext>
   )
 }
