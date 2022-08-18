c8104fedefc45432cc45128b36593ed806885240
jjangga0214 <bnbcmindnpass@gmail.com>
2022-08-18 16:28:27 +0900

    docs: fix mui theme hydration issue

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -12,15 +12,23 @@ interface NextraProps {
 }
 
 function Main({ Component, pageProps }: NextraProps): JSX.Element {
+  // Avoid Hydration Mismatch: https://github.com/pacocoursey/next-themes#avoid-hydration-mismatch
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
   // `useTheme` can only be used under `<ThemeProvider>`
   const { resolvedTheme } = useTheme()
   const theme = createTheme({
     palette: {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
-      mode: resolvedTheme,
+      mode: mounted ? resolvedTheme : 'light',
     },
   })
+
   return (
     <MuiThemeProvider theme={theme}>
       {/* eslint-disable-next-line react/jsx-props-no-spreading */}
@@ -33,26 +41,17 @@ export default function Nextra({
   Component,
   pageProps,
 }: NextraProps): JSX.Element {
-  // Avoid Hydration Mismatch: https://github.com/pacocoursey/next-themes#avoid-hydration-mismatch
-  const [mounted, setMounted] = useState(false)
-
-  useEffect(() => {
-    setMounted(true)
-  }, [])
-
-  if (mounted) {
-    return (
-      /**
-       * Nextra internally uses ThemeProvider.
-       * REF: https://github.com/shuding/nextra/blob/a8c588385f7a968125f5b9831f209292336b7674/packages/nextra-theme-docs/src/contexts/config.tsx#L47-L52
-       * Nextra theme config (e.g. theme.config.tsx) is applied to ThemeProvider.
-       * This "outer" ThemeProvider may be out of sync with the "internal" ThemeProvider.
-       * In that case, the config should be synced by hard-coding here in duplicated manner.
-       */
-      <ThemeProvider attribute="class" disableTransitionOnChange>
-        <Main Component={Component} pageProps={pageProps} />
-      </ThemeProvider>
-    )
-  }
-  return <Main Component={Component} pageProps={pageProps} />
+  return (
+    /**
+     * Nextra internally uses ThemeProvider.
+     * REF: https://github.com/shuding/nextra/blob/a8c588385f7a968125f5b9831f209292336b7674/packages/nextra-theme-docs/src/contexts/config.tsx#L47-L52
+     * Nextra theme config (e.g. theme.config.tsx) is applied to ThemeProvider.
+     * This "outer" ThemeProvider may be out of sync with the "internal" ThemeProvider.
+     * In that case, the config should be synced by hard-coding here in duplicated manner.
+     */
+    // `useTheme` can only be used under `<ThemeProvider>`. That's the reason to place <Main> in it.
+    <ThemeProvider attribute="class" disableTransitionOnChange>
+      <Main Component={Component} pageProps={pageProps} />
+    </ThemeProvider>
+  )
 }
