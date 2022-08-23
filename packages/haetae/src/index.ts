0589b6f379c1cd0c1b83e6e8b8d4f331ee6426aa
jjangga0214 <bnbcmindnpass@gmail.com>
2022-08-23 13:28:08 +0900

    refactor(haetae): use `@haetae/common`

--- packages/haetae/src/index.ts
+++ packages/haetae/src/index.ts
@@ -1,6 +1,8 @@
+import { parsePkg } from '@haetae/common'
+
 export * as core from '@haetae/core'
 export * as utils from '@haetae/utils'
 export * as js from '@haetae/javascript'
 export * as git from '@haetae/git'
 export * as cli from '@haetae/cli'
-export { default as pkg } from './pkg'
+export const pkg = parsePkg({ name: 'haetae', rootDir: __dirname })
