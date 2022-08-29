73020293cb35bc1a2bec57733134ec5c3642a0b6
jjangga0214 <bnbcmindnpass@gmail.com>
2022-08-29 19:52:47 +0900

    test(*): fix wrong tests

--- packages/cli/test/pkg.test.ts
+++
@@ -1,7 +0,0 @@
-import { AssertionError } from 'assert'
-
-describe('pkg export', () => {
-  test('without assertion error', async () => {
-    await expect(import('../src/pkg')).resolves.not.toThrow(AssertionError)
-  })
-})
--- packages/javascript/test/index.test.ts
+++ packages/javascript/test/index.test.ts
@@ -1,71 +1,146 @@
 import upath from 'upath'
 import { dependsOn } from '@haetae/javascript'
+import * as utils from '@haetae/utils'
 
 describe('dependsOn', () => {
   // TODO: add tests for other various environments
   const rootDir = upath.join(__dirname, '../../../test-project')
-  const dependOnBarIndex = dependsOn(
-    [upath.join(rootDir, 'packages/bar/src/index.ts')],
-    {
-      rootDir,
-    },
-  )
-  const dependOnFooHello = dependsOn(
-    [upath.join(rootDir, 'packages/foo/src/hello.ts')],
-    {
-      rootDir,
-    },
-  )
+
   // TODO: uncomment this test once `path mapping` is resolved by PR: https://github.com/dependents/node-dependency-tree/pull/138
   // eslint-disable-next-line jest/no-commented-out-tests
   // test('through typescript path mapping', async () => {
   //   expect(
-  //     dependOnBarIndex(
-  //       upath.join(rootDir, 'packages/bar/test/index.test.ts'),
-  //     ),
+  //     dependsOn({
+  //       rootDir,
+  //       dependent: 'packages/bar/test/index.test.ts',
+  //       dependencies: ['packages/bar/src/index.ts'],
+  //     }),
   //   ).toBe(true)
   // })
 
   test('from a same package', async () => {
     expect(
-      dependOnFooHello(upath.join(rootDir, 'packages/foo/src/index.ts')),
+      dependsOn({
+        rootDir,
+        dependent: 'packages/foo/src/index.ts',
+        dependencies: ['packages/foo/src/hello.ts'],
+      }),
     ).toBe(true)
 
     expect(
-      dependOnFooHello(
-        /**
-         * `foo/test/integration/index.test.ts` file does not use path mapping,
-         * unlike `foo/test/unit/index.test.ts`.
-         */
-        upath.join(rootDir, 'packages/foo/test/integration/index.test.ts'),
-      ),
+      /*
+       * `foo/test/integration/index.test.ts` file does not use path mapping,
+       * unlike `foo/test/unit/index.test.ts`.
+       */
+      dependsOn({
+        rootDir,
+        dependent: 'packages/foo/test/integration/index.test.ts',
+        dependencies: ['packages/foo/src/hello.ts'],
+      }),
     ).toBe(true)
   })
 
   test('from a different package', async () => {
     expect(
-      dependOnBarIndex(upath.join(rootDir, 'packages/foo/src/index.ts')),
+      dependsOn({
+        rootDir,
+        dependent: 'packages/foo/src/index.ts',
+        dependencies: ['packages/bar/src/index.ts'],
+      }),
     ).toBe(false)
+
     // TODO: uncomment these expectation once `path mapping` is resolved by PR: https://github.com/dependents/node-dependency-tree/pull/138
     // expect(
-    //   dependOnFooHello(upath.join(rootDir, 'packages/bar/src/index.ts')),
+    //   dependsOn({
+    //     rootDir,
+    //     dependent: 'packages/bar/src/index.ts',
+    //     dependencies: ['packages/foo/src/hello.ts'],
+    //   }),
     // ).toBe(true)
     // expect(
-    //   dependOnFooHello(
-    //     upath.join(rootDir, 'packages/bar/test/unit/index.test.ts'),
-    //   ),
+    //   dependsOn({
+    //     rootDir,
+    //     dependent: 'packages/bar/test/unit/index.test.ts',
+    //     dependencies: ['packages/foo/src/hello.ts'],
+    //   }),
     // ).toBe(true)
   })
 
   test('itself', async () => {
     expect(
-      dependOnBarIndex(upath.join(rootDir, 'packages/bar/src/index.ts')),
+      dependsOn({
+        rootDir,
+        dependent: 'packages/bar/src/index.ts',
+        dependencies: ['packages/bar/src/index.ts'],
+      }),
     ).toBe(true)
   })
 
   test('from a non-existent path', async () => {
     expect(
-      dependOnFooHello(upath.join(rootDir, 'path/to/non-existent.ts')),
+      dependsOn({
+        rootDir,
+        dependent: 'path/to/non-existent.ts',
+        dependencies: ['packages/foo/src/hello.ts'],
+      }),
     ).toBe(false)
+  })
+
+  test('with additional dependencies', async () => {
+    const additionalGraph = utils.graph({
+      rootDir,
+      edges: [
+        {
+          dependents: ['a'],
+          dependencies: ['b'],
+        },
+        {
+          dependents: ['c'],
+          dependencies: ['a'],
+        },
+        {
+          dependents: ['f'],
+          dependencies: ['another', 'another2'],
+        },
+      ],
+    })
+
+    const dependencies = ['f', 'b']
+
+    expect(
+      dependsOn({
+        rootDir,
+        dependent: 'a',
+        dependencies,
+        additionalGraph,
+      }),
+    ).toBe(true)
+
+    expect(
+      dependsOn({
+        rootDir,
+        dependent: 'c',
+        dependencies,
+        additionalGraph,
+      }),
+    ).toBe(true)
+
+    expect(
+      dependsOn({
+        rootDir,
+        dependent: 'e',
+        dependencies,
+        additionalGraph,
+      }),
+    ).toBe(false)
+
+    expect(
+      dependsOn({
+        rootDir,
+        dependent: 'f',
+        dependencies,
+        additionalGraph,
+      }),
+    ).toBe(true)
   })
 })
