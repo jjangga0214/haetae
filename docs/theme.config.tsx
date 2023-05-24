d666b46487815bc0e110fb1bf25e544cf21e30e1
jjangga0214 <bnbcmindnpass@gmail.com>
2023-05-25 08:38:17 +0900

    docs: partially fix head meta

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -18,15 +18,18 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="Haetae: Your Smart Incremental Tasks" />
+      <meta
+        name="og:description"
+        content="Haetae: Your Smart Incremental Tasks"
+      />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
+      <meta name="twitter:site:domain" content="https://haetae-mu.vercel.app" />
+      <meta name="twitter:url" content="https://https://haetae-mu.vercel.app" />
+      <meta name="og:title" content="Haetae: Your Smart Incremental Tasks" />
       <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="apple-mobile-web-app-title" content="Haetae" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
