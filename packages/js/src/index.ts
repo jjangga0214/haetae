d47cc75732a2986ce8d535d67150bca25f14258a
jjangga0214 <bnbcmindnpass@gmail.com>
2023-06-09 18:27:56 +0900

    fix(js): missing export of VersionOptions

--- packages/js/src/index.ts
+++ packages/js/src/index.ts
@@ -7,7 +7,7 @@ import * as core from '@haetae/core'
 import * as utils from '@haetae/utils'
 
 export { version } from './version.js'
-// export { version, VersionOptions } from './version.js' TODO: export error of interface
+export type { VersionOptions } from './version.js' // We should separate type export to avoid swc's complaining
 
 export const pkg = parsePkg({
   name: '@haetae/javascript',
