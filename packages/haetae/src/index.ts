8668329eaa438ecfa53992c7ceed4878cf02e3f1
jjangga0214 <bnbcmindnpass@gmail.com>
2023-06-09 18:27:27 +0900

    feat(haetae): rm `defaultRootRecordData`
    **BREAKING CHANGE**: `defaultRootRecordData` is removed. `@haetea/git`'s `changedFiles` calls `core.reserveRecordData` internally, so now `defaultRootRecordData` is not needed for writing `@haetea/git`s default Record Data.

--- packages/haetae/src/index.ts
+++ packages/haetae/src/index.ts
@@ -1,7 +1,5 @@
-import { parsePkg, Rec } from '@haetae/common'
+import { parsePkg } from '@haetae/common'
 import { dirname } from 'dirname-filename-esm'
-import * as core from '@haetae/core'
-import * as git from '@haetae/git'
 
 export * as core from '@haetae/core'
 export * as utils from '@haetae/utils'
@@ -9,28 +7,8 @@ export * as js from '@haetae/javascript'
 export * as git from '@haetae/git'
 export * as cli from '@haetae/cli'
 
-export { $, $Exec } from '@haetae/utils'
+export { configure } from '@haetae/core'
+export { $ } from '@haetae/utils'
+export type { $Exec } from '@haetae/utils' // We should separate type export to avoid swc's complaining
 
 export const pkg = parsePkg({ name: 'haetae', rootDir: dirname(import.meta) })
-
-export const defaultRootRecordData: core.RootRecordData<
-  Rec,
-  Rec | (Rec & git.RecordData)
-> = async (recordDataFromCommand: Rec) => {
-  if (await git.initialized()) {
-    return {
-      ...(await git.recordData()),
-      ...recordDataFromCommand,
-    }
-  }
-  return recordDataFromCommand
-}
-
-export function configure<D extends Rec, E extends Rec>(
-  haetaePreConfig: core.HaetaePreConfig,
-): core.HaetaeConfig<D, E> {
-  return core.configure({
-    recordData: defaultRootRecordData,
-    ...haetaePreConfig,
-  })
-}
