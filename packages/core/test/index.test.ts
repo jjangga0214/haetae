699bb68e109898381a59c93119513f773f8a0afa
jjangga0214 <bnbcmindnpass@gmail.com>
2023-06-18 14:06:17 +0900

    test(core): invoke*() and createRecord()

--- packages/core/test/index.test.ts
+++ packages/core/test/index.test.ts
@@ -1,11 +1,14 @@
-import { AssertionError } from 'assert/strict'
+import assert, { AssertionError } from 'assert/strict'
 import { dirname } from 'dirname-filename-esm'
 import {
   configure,
   setConfigFilename,
   localStore,
+  createRecord,
   invokeEnv,
   invokeRun,
+  invokeRootEnv,
+  invokeRootRecordData,
 } from '../src/index.js'
 
 describe('localStore()', () => {
@@ -73,65 +76,150 @@ describe('localStore()', () => {
   })
 })
 
-describe('configure()', () => {
-  describe("when commands' env and run are given with different types", () => {
-    // eslint-disable-next-line jest/require-hook
-    setConfigFilename({
-      filename: 'haetae.config.js',
-      cwd: dirname(import.meta),
-      checkExistence: false,
-    })
-    const config = configure({
-      commands: {
-        foo: {
-          env: {
-            FOO: 'foo',
-          },
-          run: async () => ({ hello: 'world' }),
+describe('configure(), invoke*(), createRecord()', () => {
+  // eslint-disable-next-line jest/require-hook
+  setConfigFilename({
+    filename: 'haetae.config.js',
+    cwd: dirname(import.meta),
+    checkExistence: false,
+  })
+  const store = localStore()
+  const config = configure({
+    env: (env, { store }) => {
+      assert(['foo', 'bar'].includes(env.envKey as string))
+      expect(store.hello).toBe('world')
+      return {
+        ...env,
+        rootEnv: true,
+      }
+    },
+    recordData: (data, { env, store }) => {
+      expect(env.rootEnv).toBe(true)
+      expect(['foo', 'bar']).toContain(env.envKey as string)
+      expect(['foo', 'bar']).toContain(data.dataKey as string)
+      expect(store.hello).toBe('world')
+      return {
+        ...data,
+        ...env,
+        rootRecordData: true,
+      }
+    },
+    commands: {
+      foo: {
+        env: {
+          envKey: 'foo',
         },
-        bar: {
-          env: () => ({
-            BAR: 'bar',
-          }),
-          run: () => ({ hello2: 'world2' }),
+        run: async ({ env, store }) => {
+          expect(env).toStrictEqual({ envKey: 'foo' })
+          expect(store.hello).toBe('world')
+          return { dataKey: 'foo' }
         },
       },
+      bar: {
+        env: async ({ store }) => {
+          expect(store.hello).toBe('world')
+          return {
+            envKey: 'bar',
+          }
+        },
+        run: ({ env, store }) => {
+          expect(env).toStrictEqual({ envKey: 'bar' })
+          expect(store.hello).toBe('world')
+          return { dataKey: 'bar' }
+        },
+      },
+    },
+    store: {
+      ...store,
+      hello: 'world',
+    },
+  })
+  test('when `env` is given as an object', async () => {
+    await expect(
+      invokeEnv({
+        command: 'foo',
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        config,
+      }),
+    ).resolves.toStrictEqual({
+      envKey: 'foo',
     })
-    test('when env is given as an object', async () => {
-      await expect(
-        invokeEnv({
-          command: 'foo',
-          config,
-        }),
-      ).resolves.toStrictEqual({
-        FOO: 'foo',
-      })
+  })
+  test('when `env` is given as a function', async () => {
+    await expect(
+      invokeEnv({
+        command: 'bar',
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        config,
+      }),
+    ).resolves.toStrictEqual({
+      envKey: 'bar',
     })
-    test('when env is given as a function', async () => {
-      await expect(
-        invokeEnv({
-          command: 'bar',
-          config,
-        }),
-      ).resolves.toStrictEqual({
-        BAR: 'bar',
-      })
+  })
+  test('when `run` is given as an async function', async () => {
+    await expect(
+      invokeRun({
+        command: 'foo',
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        config,
+      }),
+    ).resolves.toStrictEqual({ dataKey: 'foo' })
+  })
+  test('when `run` is given as a synchrounous function', async () => {
+    await expect(
+      invokeRun({
+        command: 'bar',
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        config,
+      }),
+    ).resolves.toStrictEqual({ dataKey: 'bar' })
+  })
+  // eslint-disable-next-line jest/prefer-lowercase-title
+  test('invokeRootEnv()', async () => {
+    await expect(
+      invokeRootEnv({
+        env: { envKey: 'foo' },
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        config,
+      }),
+    ).resolves.toStrictEqual({ envKey: 'foo', rootEnv: true })
+  })
+  // eslint-disable-next-line jest/prefer-lowercase-title
+  test('invokeRootRecordData()', async () => {
+    await expect(
+      invokeRootRecordData({
+        env: { envKey: 'foo', rootEnv: true },
+        recordData: { dataKey: 'foo' },
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        config,
+      }),
+    ).resolves.toStrictEqual({
+      envKey: 'foo',
+      dataKey: 'foo',
+      rootRecordData: true,
+      rootEnv: true,
     })
-    test('when run is given as an async function', async () => {
-      await expect(
-        invokeRun({
-          command: 'foo',
-          config,
-        }),
-      ).resolves.toStrictEqual({ hello: 'world' })
-    })
-    test('when run is given as a synchrounous function', async () => {
-      await expect(
-        invokeRun({
-          command: 'bar',
-          config,
-        }),
-      ).resolves.toStrictEqual({ hello2: 'world2' })
+  })
+  test('createRecord()', async () => {
+    await expect(
+      createRecord({
+        env: { envKey: 'foo', rootEnv: true },
+        recordData: { dataKey: 'foo' },
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        config,
+      }),
+    ).resolves.toStrictEqual({
+      envKey: 'foo',
+      dataKey: 'foo',
+      rootRecordData: true,
+      rootEnv: true,
     })
   })
 })
