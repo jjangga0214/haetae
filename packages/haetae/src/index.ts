e05e4cc1b8e84049ccefca70bf99c3bcc8946801
jjangga0214 <bnbcmindnpass@gmail.com>
2023-07-02 12:44:27 +0900

    feat(haetae): export configure

--- packages/haetae/src/index.ts
+++ packages/haetae/src/index.ts
@@ -1,5 +1,6 @@
 import { parsePkg } from '@haetae/common'
 import { dirname } from 'dirname-filename-esm'
+import * as core from '@haetae/core'
 
 export * as core from '@haetae/core'
 export * as utils from '@haetae/utils'
@@ -7,9 +8,9 @@ export * as js from '@haetae/javascript'
 export * as git from '@haetae/git'
 export * as cli from '@haetae/cli'
 
-export { configure } from '@haetae/core'
-
 export { $ } from '@haetae/utils'
 export type { $Exec } from '@haetae/utils' // We should separate type export to avoid swc's complaining
 
 export const pkg = parsePkg({ name: 'haetae', rootDir: dirname(import.meta) })
+
+export const { configure } = core
